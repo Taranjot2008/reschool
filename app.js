ca6e7a5136ac2ce38ff5a76683f4fed4003b29e8
Taranjot2008 <taranjotscience@gmail.com>
2025-05-12 19:08:31 -0300

    update

---
+++ app.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+})
+
+const back_top = document.querySelector("#back_top_btn")
+
+window.addEventListener("scroll", () =>{
+
+    if (window.scrollY > 2000){
+        back_top.classList.add('show')
+    }
+
+    else{
+        back_top.classList.remove('show')
+    }
+})
+
+back_top.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+
+    window.open('info.html', '_blank')
+})
