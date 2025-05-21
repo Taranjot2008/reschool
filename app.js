04ddb000fe6f9f71b94dd44628708c289decc3f0
Taranjot2008 <taranjotscience@gmail.com>
2025-05-21 19:26:10 -0300

    temporary update

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const popup = document.querySelector(".temporary_update")
+
 document.addEventListener("DOMContentLoaded", () => {
 
+    popup.style.display = "flex";
+
+    document.body.style.overflow = "hidden"
 })
 
 const back_top = document.querySelector("#back_top_btn")
