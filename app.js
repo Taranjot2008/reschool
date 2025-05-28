0d47fa9ee9b4b4443f9f3f9ac5745dc6a33a4399
Taranjot2008 <taranjotscience@gmail.com>
2025-05-27 21:00:34 -0300

    translation in french and responsive

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 const back_top = document.querySelector("#back_top_btn")
+const back_top_fr = document.querySelector("#back_top_btn_fr");
 
 window.addEventListener("scroll", () =>{
 
@@ -20,6 +21,62 @@ window.addEventListener("scroll", () =>{
     }
 })
 
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 2000){
+        back_top_fr.classList.add('visible')
+    }
+
+    else{
+        back_top_fr.classList.remove('visible')
+    }
+})
+
+const language = document.getElementById('lang_select');
+
+language.addEventListener('change', () => {
+    
+    switch (language.value){
+        case 'eng':
+            window.location.href = '../english/home.html'
+            break;
+        
+        case 'fre':
+            window.location.href = '../french/fr_home.html'
+            break;
+        
+        default:
+            
+            break;
+    }
+})
+
+
+const currentpage = window.location.pathname.split("/").pop();
+let currentValue = "";
+
+switch (currentpage){
+    case 'home.html':
+        currentValue = "eng";
+        break;
+
+    case 'fr_home.html':
+        currentValue = "fre";
+        break;
+
+    default:
+        break;
+}
+
+
+for (let option of language.options){
+    if (option.value === currentValue){
+        option.selected = true;
+        break;
+    }
+}
+
+
 back_top.addEventListener("click", () => {
 
     window.scrollTo({
@@ -27,5 +84,6 @@ back_top.addEventListener("click", () => {
         behavior: "smooth"
     })
 
-    window.open('info.html', '_blank')
+    window.open('../english/info.html', '_blank')
 })
+
